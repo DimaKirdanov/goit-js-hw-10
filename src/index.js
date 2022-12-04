f0690a1b5cb7090a53285a0c07b196ef19530bfa
DimaKirdanov <108092906+DimaKirdanov@users.noreply.github.com>
2022-12-04 09:07:55 +0200

    add lib

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+fetch('https://restcountries.com/v2/name/peru').then((response) => {
+    return response.json();
+}).then((country) => {
+    console.log(country);
+    const markup = countryCardTemplate(country);
+    console.log(country);
+}).catch((error) => { 
+    console.log(error);
+});
+
+
