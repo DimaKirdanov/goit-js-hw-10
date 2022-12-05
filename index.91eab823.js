49cdb788f2f51694733f54e694d134f7deec99bd
DimaKirdanov <108092906+DimaKirdanov@users.noreply.github.com>
2022-12-05 18:38:19 +0000

    Deploying to gh-pages from @ DimaKirdanov/goit-js-hw-10@80794cd3327be41b3222e988439edb4a2fef569e 🚀

--- index.91eab823.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/name/peru").then((function(n){return n.json()})).then((function(n){console.log(n),countryCardTemplate(n),console.log(n)})).catch((function(n){console.log(n)}));
-//# sourceMappingURL=index.91eab823.js.map
--- index.b1950979.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/name/peru").then((o=>o.json())).then((o=>{console.log(o);countryCardTemplate(o);console.log(o)})).catch((o=>{console.log(o)}));
-//# sourceMappingURL=index.b1950979.js.map
